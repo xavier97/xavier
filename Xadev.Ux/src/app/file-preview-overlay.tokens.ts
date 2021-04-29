4f0d8eadb1b46acaa737eba02b51cbd4f451021e
Xavier <xad2@pitt.edu>
2021-04-28 22:35:34 -0400

    Update .gitignore and clean tracked files

--- Xadev.Ux/src/app/file-preview-overlay.tokens.ts
+++
@@ -1,4 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { Image } from './file-preview-overlay.service';
-
-export const FILE_PREVIEW_DIALOG_DATA = new InjectionToken<Image>('FILE_PREVIEW_DIALOG_DATA');
